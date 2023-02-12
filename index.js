d56f13285df47b5a92e24a03978b227c53a0c8db
Miss Elysea <elyseacrawford@gmail.com>
2023-02-12 13:46:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const input = 'turpentine and turtles'
+const vowels = ['a', 'e', 'i', 'o', 'u']
+const resultArray = []
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+// console.log(inputIndex)
+  if (input[inputIndex] === 'e') {
+    resultArray.push(input[inputIndex]);
+  }
+  if (input[inputIndex] === 'u') {
+    resultArray.push(input[inputIndex]);
+  }
+  for (let vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex++) {
+// console.log(vowelsIndex)
+    if (input[inputIndex] === vowels[vowelsIndex]){
+// console.log(input[inputIndex])
+     resultArray.push(input[inputIndex]);
+    }
+  }
+}
+
+// console.log(resultArray);
+
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString);
